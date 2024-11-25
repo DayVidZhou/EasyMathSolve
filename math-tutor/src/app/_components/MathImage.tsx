cfea7bae8633cc12c2eda4794661914a0a6d23b0
DayVidZhou <dzhou.1996@gmail.com>
2024-11-24 23:01:27 -0500

    added steps support

--- math-tutor/src/app/_components/MathImage.tsx
+++ math-tutor/src/app/_components/MathImage.tsx
@@ -4,14 +4,17 @@ import React, { useState } from "react";
 import { api } from "~/trpc/react";
 import * as Tesseract from "tesseract.js";
 
+interface MathSolve {
+  firstNumber: number;
+  secondNumber: number;
+  steps: Array<string>;
+}
+
 export const MathImage: React.FC = () => {
-  const [isPreview, setIsPreview] = useState(true);
-  const [selectedImage, setSelectedImage] = useState<File | undefined>(
-    undefined,
-  );
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
   const [previewUrl, setPreviewUrl] = useState<string | null>(null);
-  const utils = api.useUtils();
-  const extractTextMutation = api.post.solveMath.useMutation();
+  const [solveSteps, setSolveSteps] = useState<string[] | null>(null);
+  const solveMathMutation = api.post.solveMath.useMutation();
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files?.[0]) {
@@ -31,50 +34,74 @@ export const MathImage: React.FC = () => {
     }
 
     try {
-      const extractedText = (await Tesseract.recognize(selectedImage)).data.text;
-      if(!extractedText) {
+      const extractedText = (await Tesseract.recognize(selectedImage)).data
+        .text;
+      if (!extractedText) {
         throw new Error("No text was found in the image");
       }
 
-      console.log("extractedText", extractedText)
-      const solveSteps = await extractTextMutation.mutateAsync({text: extractedText});
-      console.log("steps",solveSteps);
-
+      const solveObjString = await solveMathMutation.mutateAsync({
+        text: extractedText,
+      });
+      let solvedObj: MathSolve = JSON.parse(solveObjString.solution);
+      console.log({ solvedObj });
+      let tempSteps = solvedObj.steps.slice(0, solvedObj.steps.length - 1);
+      tempSteps.push(
+        `The result should be ${solvedObj.firstNumber * solvedObj.secondNumber}`,
+      );
+      setSolveSteps(tempSteps);
     } catch (error) {
-      console.error("Error extracting text:", error);
       alert("Failed to extract text from the image.");
     }
   };
 
   return (
-    <div className="flex flex-col space-y-4 h-1/2  w-full">
-      <div className=" min-h-28 content-center justify-center  bg-indigo-950">
+    <div className="flex h-1/2 w-full flex-col space-y-4">
+      <h1 className="text-2xl">This only works for 2 value multiplication problems!</h1>
+      <div className="min-h-28 content-center justify-center bg-indigo-950">
         {!previewUrl ? (
           <>
-            <div className="flex-col flex h-full w-full justify-center m-auto text-center">
-            <label>Upload an image to preview it here</label>
+            <div className="m-auto flex h-full w-full flex-col justify-center text-center">
+              <label>Upload an image to preview it here</label>
             </div>
           </>
         ) : (
           <img
             src={previewUrl}
             alt="Selected"
-            className="h-full w-full max-w-96 m-auto object-contain"
+            className="m-auto h-full w-full max-w-96 object-contain"
           />
         )}
       </div>
-      <div className="mt-10"/>
+      <div className="mt-10" />
 
       <input
-            type="file"
-            accept="image/*"
-            onChange={handleFileChange}
-            className="mt-2 m-auto"
-          />
-      <div className="mt-10"/>
-      <button onClick={handleExtractText} className="m-auto w-1/2 rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20">
+        type="file"
+        accept="image/*"
+        onChange={handleFileChange}
+        className="m-auto mt-2"
+      />
+      <div className="mt-10" />
+      <button
+        disabled={solveMathMutation.isPending}
+        onClick={handleExtractText}
+        className="m-auto w-1/2 rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
+      >
         <span>Submit Chosen Image</span>
       </button>
+      <div className="mt-10" />
+      {solveSteps ? (
+        <div>
+          <h2 className="text-lg" >Steps to solve the problem</h2>
+          <div className="mt-5" />
+          {solveSteps.map((val, i) => (
+            <>
+              <p key={i}>{val}</p>
+              <div className="mt-5" />
+            </>
+          ))}
+        </div>
+      ) : null}
     </div>
   );
 };
